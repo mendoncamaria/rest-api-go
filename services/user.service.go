7ec2b9e3135e1e211d018789504840850a7f677b
Maria Mendonca <maria.mendonca@niveussolutions.com>
2024-09-20 10:56:39 +0530

    base structure

---
+++ services/user.service.go
@@ -0,0 +1,11 @@
+package services
+
+import "rest-api-go/models"
+
+type userService interface {
+	CreateUser(*models.User) error
+	GetUser(*string) (*models.User, error)
+	GetAll() ([]*models.User, error)
+	UpdateUser(*models.User) error
+	DeleteUser(*string) error
+}
