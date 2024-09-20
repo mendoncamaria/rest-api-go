f00ac623391daf7536081c1f179f27820c660f3e
Maria Mendonca <maria.mendonca@niveussolutions.com>
2024-09-20 11:18:12 +0530

    base commit controller

---
+++ controllers/user.controller.go
@@ -0,0 +1,46 @@
+package controllers
+
+import (
+	"rest-api-go/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserController struct {
+	userService services.UserService
+}
+
+func New(userservice services.UserService) UserController {
+	return UserController{
+		userService: userservice,
+	}
+}
+
+func (uc *UserController) CreateUser(ctx *gin.Context) {
+	ctx.JSON(200, "")
+}
+
+func (uc *UserController) GetUser(ctx *gin.Context) {
+	ctx.JSON(200, "")
+}
+
+func (uc *UserController) GetAll(ctx *gin.Context) {
+	ctx.JSON(200, "")
+}
+
+func (uc *UserController) UpdateUser(ctx *gin.Context) {
+	ctx.JSON(200, "")
+}
+
+func (uc *UserController) DeleteUser(ctx *gin.Context) {
+	ctx.JSON(200, "")
+}
+
+func (uc *UserController) RegisterUserRoutes(rg *gin.RouterGroup) {
+	userroute := rg.Group("/user")
+	userroute.POST("/create", uc.CreateUser)
+	userroute.GET("/get/:name", uc.GetUser)
+	userroute.GET("/getall", uc.GetAll)
+	userroute.PATCH("/update", uc.UpdateUser)
+	userroute.DELETE("/delete/:name", uc.DeleteUser)
+}
